37525add6b02a38edfcb1191eb417ad118a2bdbf
Kavikumar M <kavikumarceo@gmail.com>
2025-01-05 17:10:20 +0530

    front

--- frontend/src/pages/Search.jsx
+++ frontend/src/pages/Search.jsx
@@ -3,10 +3,13 @@ import { useEffect, useState } from 'react';
 import { CiSearch } from 'react-icons/ci';
 import { IoPersonAdd } from 'react-icons/io5';
 import { AiFillStar, AiOutlineStar } from 'react-icons/ai';
+import useUserStore from '../../stores/useUserStore';
 
 function Search() {
   const [users, setUsers] = useState([]);
   const [searchTerm, setSearchTerm] = useState(''); // State for search input
+  const { githubData } = useUserStore();
+  // console.log(githubData);
 
   useEffect(() => {
     const findUsers = async () => {
@@ -57,37 +60,40 @@ function Search() {
         </div>
       </div>
       <div className="mt-8 w-full max-w-2xl z-20 flex flex-col items-center justify-center gap-1">
-        {filteredUsers.map((user) => (
-          <div
-            key={user._id}
-            className="flex w-full items-center justify-between border shadow-sm rounded-md p-2 my-2 hover:scale-105 duration-200 cursor-pointer hover:shadow-md hover:shadow-slate-400"
-          >
-            <div className="flex items-center gap-2">
-              <img
-                src={user.profilePicture}
-                alt={user.name}
-                className="rounded-md h-24 w-24"
-              />
-              <div className="flex flex-col items-start justify-center gap-1">
-                <div className="flex items-center gap-3">
-                  <h2 className="text-white text-xl font-semibold">
-                    {user.username}
-                  </h2>
-                  {/* Render dynamic star rating */}
-                  <div className="flex">{renderStars(user.rating)}</div>
+        {filteredUsers.map(
+          (user) =>
+            user.githubID != githubData?.username && (
+              <div
+                key={user._id}
+                className="flex w-full items-center justify-between border shadow-sm rounded-md p-2 my-2 hover:scale-105 duration-200 cursor-pointer hover:shadow-md hover:shadow-slate-400"
+              >
+                <div className="flex items-center gap-2">
+                  <img
+                    src={user.profilePicture}
+                    alt={user.name}
+                    className="rounded-md h-24 w-24"
+                  />
+                  <div className="flex flex-col items-start justify-center gap-1">
+                    <div className="flex items-center gap-3">
+                      <h2 className="text-white text-xl font-semibold">
+                        {user.username}
+                      </h2>
+                      {/* Render dynamic star rating */}
+                      <div className="flex">{renderStars(user.rating)}</div>
+                    </div>
+                    <h2 className="text-md font-light text-gray-300">
+                      ID: {user.githubID}
+                    </h2>
+                    <p className="text-gray-300">{user.email}</p>
+                  </div>
                 </div>
-                <h2 className="text-md font-light text-gray-300">
-                  ID: {user.githubID}
-                </h2>
-                <p className="text-gray-300">{user.email}</p>
-              </div>
-            </div>
 
-            <button className="text-white p-4 hover:text-green-600 duration-150 rounded-md flex items-center gap-1">
-              <IoPersonAdd className="h-6 w-6" />
-            </button>
-          </div>
-        ))}
+                <button className="text-white p-4 hover:text-green-600 duration-150 rounded-md flex items-center gap-1">
+                  <IoPersonAdd className="h-6 w-6" />
+                </button>
+              </div>
+            )
+        )}
       </div>
     </div>
   );
